be96f489d339484f0cca0a9f6a9e980d2e140f55
wxlund <wxlund@yahoo.com>
2016-11-17 15:44:24 -0800

    removing manifest.*.yml files accept for manifest-pcfdev for security reasons

--- pws-scdf-setup.sh
+++ pws-scdf-setup.sh
@@ -10,31 +10,27 @@ echo "                                                                          
 echo "                                                                                          ";
 echo "                                                                                          ";
 if [ $# -eq 0 ]; then
-	echo "Usage: pcfdev-scdf-setup.sh pcfdev-org pcfdev-space admin admin";
+	echo "Usage: demo_setup_1.sh <organization> <space> <email> <password>";
 	echo "Program terminating ...";
 	exit 1;
 fi
 
-APP_NAME=$3-dataflow-server
+APP_NAME=$2-dataflow-server
 
 #echo "Setting the end point for PWS"
 #cf api https://api.run.pivotal.io
 
 echo "Running: cf login"
-cf login -a https://api.local.pcfdev.io --skip-ssl-validation -u "$3" -p "$4" -o "$1" -s "$2"  
-
-p-mysql      512mb, 1gb   MySQL databases on demand
-p-rabbitmq   standard     RabbitMQ is a robust and scalable high-performance multi-protocol messaging broker.
-p-redis      shared-vm    Redis service to provide a key-value store
+cf login -a https://api.run.pivotal.io -u "$3" -p "$4" -o "$1" -s "$2" 
 
 echo "Creating the required Redis Service"
-cf create-service p-redis shared-vm redis
+cf create-service rediscloud 30mb redis
 
 echo "Creating the required Rabbit Service"
-cf create-service p-rabbitmq standard  rabbit
+cf create-service cloudamqp lemur rabbit
 
 echo "Creating the required MySql Service"
-cf create-service p-mysql 512mb  mysql
+cf create-service cleardb spark mysql
 
 if [ ! -f spring-cloud-dataflow-server-cloudfoundry-1.0.1.RELEASE.jar ]; then
 	echo "Downloading the Server App for Pivotal Cloud Foundry. This will be deployed in Cloud Foundry"
@@ -60,26 +56,18 @@ cf bind-service $APP_NAME mysql
 
 echo "Setting the environmental variables"
 cf set-env $APP_NAME MAVEN_REMOTE_REPOSITORIES_REPO1_URL https://repo.spring.io/libs-snapshot
-cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_URL https://api.local.pcfdev.io
+cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_URL https://api.run.pivotal.io
 cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_ORG "$1"
 cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_SPACE $2
-cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_DOMAIN pcfdev.io
-cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_STREAM_SERVICES rabbit, redis
+cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_DOMAIN cfapps.io
+cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_STREAM_SERVICES rabbit
 cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_USERNAME $3
 cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_PASSWORD $4
-cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_SKIP_SSL_VALIDATION true
-cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_TASK_SERVICES redis,rabbit, mysql
-cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_TASK_MEMORY 512
-cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_STREAM_MEMORY 512
-cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_STREAM_DISK 1024
-cf set-env $APP_NAME SPRING_CLOUD_DATAFLOW_FEATURES_EXPERIMENTAL_TASKSENABLED true
-cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_TASK_MEMORY: 512
-cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_TASK_DISK: 512
-cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_TASK_INSTANCES: 1
-cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_TASK_BUILDPACK: java_buildpack
+cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_SKIP_SSL_VALIDATION false
+cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_SERVICES redis,rabbit
+cf set-env $APP_NAME SPRING_CLOUD_DATAFLOW_FEATURES_EXPERIMENTAL_TASKSENABLED: true
 
 echo "Starting the Server"
 cf start $APP_NAME
 
 echo "Set Up Complete"
-
